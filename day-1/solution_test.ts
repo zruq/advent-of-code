5a6ed8e26f14883d5fc000624393276793202293
zruq <zruq@proton.me>
2024-12-01 12:33:04 +0100

    day 1

---
+++ day-1/solution_test.ts
@@ -0,0 +1,46 @@
+import { assertEquals } from "@std/assert";
+import {
+  calculateSimilarityScore,
+  calculateTotalDistance,
+  parseInput,
+} from "./solution.ts";
+
+Deno.test(async function parseInputTest() {
+  const result = await parseInput("./day-1/test-input.txt");
+  assertEquals(result, [[3, 4, 2, 1, 3, 3], [
+    4,
+    3,
+    5,
+    3,
+    9,
+    3,
+  ]]);
+});
+
+Deno.test(function findTotalDistanceTest() {
+  assertEquals(
+    calculateTotalDistance([3, 4, 2, 1, 3, 3], [
+      4,
+      3,
+      5,
+      3,
+      9,
+      3,
+    ]),
+    11,
+  );
+});
+
+Deno.test(function findTotalDistanceTest() {
+  assertEquals(
+    calculateSimilarityScore([3, 4, 2, 1, 3, 3], [
+      4,
+      3,
+      5,
+      3,
+      9,
+      3,
+    ]),
+    31,
+  );
+});
