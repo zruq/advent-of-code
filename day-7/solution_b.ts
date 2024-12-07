c0aa03a2fea81389a10ced7d392b715035c7ec1f
zruq <zruq@proton.me>
2024-12-07 10:49:02 +0100

    day 7 part b

---
+++ day-7/solution_b.ts
@@ -0,0 +1,59 @@
+type Data = { expected: number; values: Array<number> };
+type Operation = (a: number, b: number) => number;
+
+export function parseInput(input: string): Array<Data> {
+  return input.split("\n").reduce<Array<Data>>((p, c) => {
+    const [expected, valuesString] = c.split(":");
+    p.push({
+      expected: Number(expected),
+      values: valuesString.trim().split(" ").map((x) => Number(x)),
+    });
+    return p;
+  }, []);
+}
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function concat(a: number, b: number): number {
+  return Number(a.toString() + b.toString());
+}
+
+const ALLOWED_OPREATIONS: Array<Operation> = [add, multiply, concat];
+
+const COMBINATIONS_DICT: Record<number, Array<Array<Operation>>> = {};
+
+function getCombinations(n: number) {
+  if (COMBINATIONS_DICT[n]) {
+    return COMBINATIONS_DICT[n];
+  }
+  const combinations: Array<Array<Operation>> = [];
+  for (let i = 0; i < 3 ** n; i++) {
+    const combination: Array<Operation> = i.toString(3).padStart(n, "0").split(
+      "",
+    ).map((a) => ALLOWED_OPREATIONS[Number(a)]);
+    combinations.push(combination);
+  }
+  COMBINATIONS_DICT[n] = combinations;
+  return combinations;
+}
+
+export function isEquationTrue(data: Data): boolean {
+  const combinations = getCombinations(data.values.length - 1);
+  return combinations.some((combination) => {
+    const values = [...data.values];
+    const operations = [...combination];
+    while (values.length > 1 && operations.length) {
+      const operation = operations.shift()!;
+      const a = values.shift()!;
+      const b = values.shift()!;
+      values.unshift(operation(a, b));
+    }
+    return values[0] === data.expected;
+  });
+}
